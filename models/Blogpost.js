ff77f74fec76138e40fd4b470465fcf37e1991bc
rangamboa <rangamboa@gmail.com>
2021-08-03 18:23:30 -0700

    testing model

--- models/Blogpost.js
+++ models/Blogpost.js
@@ -15,17 +15,17 @@ Blogpost.init(
       type: DataTypes.STRING,
       allowNull: false,
     },
-    date_created: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      defaultValue: DataTypes.NOW,
-    },
     user_id: {
       type: DataTypes.INTEGER,
       references: {
         model: 'user',
         key: 'id',
       },
+    },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
     },
   },
   {
