7c935e01ee882d67ff7f4bb35b7520104dd12f5c
rangamboa <rangamboa@gmail.com>
2021-08-03 18:49:05 -0700

    debugging handlebars

--- controllers/mainRoutes.js
+++ controllers/mainRoutes.js
@@ -4,10 +4,12 @@ const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
-    res.render('test', { 
-      blogposts, 
-      logged_in: req.session.logged_in 
-    });
+    res.render('main', {layout: 'index' }); 
+
+    // res.render('index', { 
+    //   blogposts, 
+    //   logged_in: req.session.logged_in 
+    // });
 
   } catch (err) {
     res.status(500).json(err);
--- server.js
+++ server.js
@@ -28,8 +28,12 @@ const sess = {
 app.use(session(sess));
 
 // Inform Express.js on which template engine to use
-app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+// app.engine('handlebars', hbs.engine);
+
+app.engine('handlebars', exphbs({
+  layoutsDir: __dirname + '/views/layouts',
+  }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
